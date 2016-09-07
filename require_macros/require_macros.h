103569fb01e4a8060493181f6a999dd67dcb104e
Felix Wong <Felix Wong>
2016-09-07 17:17:39 -0400

    Added PlatformStatus and require_macros. Returning PlatformStatus with GPIO functions.

---
+++ require_macros/require_macros.h
@@ -0,0 +1,70 @@
+/*
+ * require_macros.h
+ *
+ * Created: 2016-09-07 4:46:14 PM
+ *  Author: Felix
+ */ 
+
+
+#ifndef REQUIRE_MACROS_H_
+#define REQUIRE_MACROS_H_
+
+#define USE_PRODUCTION_CODE 1
+
+#if USE_PRODUCTION_CODE
+#define require( assertation, label )             \
+	do                                            \
+	{                                             \
+		if ( __builtin_expect( !assertation, 0 )) \
+		{                                         \
+			goto label;                           \
+		}                                         \
+	}                                             \
+	while ( 0 )
+#else
+#define require( assertation, label ) //TODO                   
+#endif // USE_DEBUG_CODE
+
+#if USE_PRODUCTION_CODE
+#define require_noerr( err, label )         \
+	do                                      \
+	{                                       \
+		if ( __builtin_expect( err != 0  )) \
+		{                                   \
+			goto label;                     \
+		}                                   \
+	}                                       \
+	while ( 0 )
+#else
+#define require_noerr( err, label ) //TODO
+#endif // USE_DEBUG_CODE
+
+#if USE_PRODUCTION_CODE
+#define require_quiet( assertation, label )           \
+	do                                                \
+	{                                                 \
+		if ( __builtin_expect( !assertation, false )) \
+		{                                             \
+			goto label;                               \
+		}                                             \
+	}                                                 \
+	while ( 0 )
+#else
+#define require_quiet( err, label ) //TODO
+#endif // USE_DEBUG_CODE
+
+#if USE_PRODUCTION_CODE
+#define require_noerr_quiet( err, label )         \
+	do                                            \
+	{                                             \
+		if ( __builtin_expect( err != 0, false )) \
+		{                                         \
+			goto label;                           \
+		}                                         \
+	}                                             \
+	while ( 0 )
+#else
+#define require_noerr_quiet( err, label ) //TODO
+#endif // USE_DEBUG_CODE
+
+#endif /* REQUIRE_MACROS_H_ */
